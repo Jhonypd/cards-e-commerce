df7d0625e7fa958cb7f532d466347a4324bcbb07
Jhony Pereira Duarte <jhony-16@live.com>
2023-03-02 01:21:59 -0300

    atualização, modo escuro add, footer e menu, mais ards

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
+const toggleThemeMode = () => {
+    body.classList.toggle("dark");
+    header.classList.toggle("neon");
+};
+
+input.onchange = toggleThemeMode;
+
+
 const caixaImagem = document.querySelectorAll('.caixa-img');
 
 caixaImagem.forEach(function(caixa){
